f2f32f05918099cd943ded1236acc026059398c3
sukhvinder-jaat <monudhaka829@gmail.com>
2023-12-08 12:12:50 +0530

    first commit

---
+++ src/components/Practise.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Practise = () => {
+  return (
+    <>
+      <p className=" text-center text-[100px] text-red-300">Practise</p>
+    </>
+  );
+};
+
+export default Practise;
